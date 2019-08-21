3059ac0633e57413ec737e28b908d5b2ae4ca050
Steven Martin <smarti24@jaguarlandrover.com>
2019-08-21 19:47:47 +0000

    Tweaks to how program gets executed

--- print_name_and_age_client.cpp
+++ print_name_and_age_client.cpp
@@ -1,17 +1,9 @@
 #include "dstc.hpp"
 #include <iostream>
+#include <atomic>
 
 // Namespace is optional, but its convenient to put all of the DSTC functions in the same place
 namespace Remote {
-    // This may change slightly, but I think it is easiest for the DSTC name to live in global memory because
-    // it is the "ID" that DSTC knows about a specific remote call
-    //const char print_name_and_age_dstc_name[] = "print_name_and_age";
-
-    // We create a callable object that can be invoked to execute the remote function
-    //dstc::RemoteFunction <print_name_and_age_dstc_name, char[32], int> printNameAndAge;
-
-    //const char test_dynamic_function_name[] = "test_dynamic_function";
-    //dstc::RemoteFunction<test_dynamic_function_name, dstc_dynamic_data_t, int[4]> testDynamicFunctionName;
 
     namespace names {
         const char double_value_name[] = "double_value";
@@ -21,46 +13,20 @@ namespace Remote {
 }
 
 int main() {
-
-    // Wait for server to come alive
-    //Remote::printNameAndAge.blockUntilServerAvailable();
-    //std::cout << "Waiting for server function..." << std::endl;
-    //Remote::testDynamicFunctionName.blockUntilServerAvailable();
-    //std::cout << "Ready." << std::endl;
-
-    //char payload[] = "blah blah data blammo this thing";
-    //dstc_dynamic_data_t data;
-    //data.data = (void*)payload;
-    //data.length = strlen(payload) + 1;
-
-    //int array[4] = {2, 4, 6, 8};
-
-    //Remote::testDynamicFunctionName(data, array);
-
-    // Queue DSTC command
-    //char name[] = "Jack O'Lope";
-    ///Remote::printNameAndAge(name, 823);
-
     std::cout << "Waiting for server function..." << std::endl;
     Remote::doubleValue.blockUntilServerAvailable();
     std::cout << "Server function available." << std::endl;
 
-    /*
-    for (auto i = 0; i < 64; ++i) {
-        Remote::doubleValue(i,
-            dstc::CallbackFunction<int>(
-                [](int result) { std::cout << "Got value: " << result; }
-            )
-        );
-    }
-    */
+    std::atomic<bool> flag(false);
 
-
-        Remote::doubleValue(12345, dstc::CallbackFunction<int>(
-            [](int result) { std::cout << "Got value: " << result << std::endl;; }
-        ));
-
-        while(true) {
-            dstc_process_events(-1);
+    Remote::doubleValue(12345, dstc::CallbackFunction<int>(
+        [&flag](int result) {
+            std::cout << "Got value: " << result << std::endl;
+            flag = true;
         }
+    ));
+
+    while(!flag) {
+        dstc_process_events(-1);
+    }
 }
